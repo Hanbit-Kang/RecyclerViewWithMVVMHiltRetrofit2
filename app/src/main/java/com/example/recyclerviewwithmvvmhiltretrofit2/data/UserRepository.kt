ebc87ee34a676f7a77422f8cd43b04931b929ec6
Hanbit-Kang <fchopinof99@gmail.com>
2022-06-27 15:25:52 +0900

    Add UserRepository

---
+++ app/src/main/java/com/example/recyclerviewwithmvvmhiltretrofit2/data/UserRepository.kt
@@ -0,0 +1,12 @@
+package com.example.recyclerviewwithmvvmhiltretrofit2.data
+
+import com.example.recyclerviewwithmvvmhiltretrofit2.api.ReqresApi
+import javax.inject.Inject
+
+class UserRepository @Inject constructor(
+    private val reqresApi: ReqresApi
+){
+    suspend fun getUserList(page: Int): UserListResponse? {
+        return reqresApi.getUserList(page).body()
+    }
+}
