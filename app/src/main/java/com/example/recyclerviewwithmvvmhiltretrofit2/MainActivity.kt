86f0c713247631ef270f527d43b0b37ea78564c7
Hanbit-Kang <fchopinof99@gmail.com>
2022-06-27 14:24:37 +0900

    Modify package name

--- app/src/main/java/com/example/recyclerviewwithmvvmhiltretrofit2/MainActivity.kt
+++ app/src/main/java/com/example/recyclerviewwithmvvmhiltretrofit2/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.myapplication
+package com.example.recyclerviewwithmvvmhiltretrofit2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
--- app/src/test/java/com/example/recyclerviewwithmvvmhiltretrofit2/ExampleUnitTest.kt
+++ app/src/test/java/com/example/recyclerviewwithmvvmhiltretrofit2/ExampleUnitTest.kt
@@ -1,4 +1,4 @@
-package com.example.myapplication
+package com.example.recyclerviewwithmvvmhiltretrofit2
 
 import org.junit.Test
 
