60221a55a157ea0d2d8dfc93a54893b59ab1c9bd
hanbikan <fchopinof99@gmail.com>
2023-03-08 16:20:39 +0900

    Modify project gradle to use libs

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,14 +5,10 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:7.3.1")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
-        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.4.1")
-        classpath("com.google.dagger:hilt-android-gradle-plugin:2.41")
-        /*classpath(libs.gradle)
+        classpath(libs.gradle)
         classpath(libs.kotlin.gradle.plugin)
         classpath(libs.navigation.safe.args.gradle.plugin)
-        classpath(libs.android.gradle.plugin)*/
+        classpath(libs.hilt.android.gradle.plugin)
     }
 }
 
